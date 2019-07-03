b493274c4247cafa2bb22e8867e1228b4dc3cf26
Buvaneshwari-m <52272256+Buvaneshwari-m@users.noreply.github.com>
2019-07-03 14:41:35 +0530

    Update 22.py

--- 22.py
+++ 22.py
@@ -1,5 +1,5 @@
-h=int(input())
-arr=[]
-for i in range(h):
-   arr=map(int,input().split())
-   print(max(arr))
+buv=int(input())
+ifd=[]
+for j in range(buv):
+   ifd=map(int,input().split())
+   print(max(ifd))
